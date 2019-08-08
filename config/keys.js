624b9db3548c1b974eed3724fc13fb20446548fd
Rakshit HR <rakshithgowdahr@gmail.com>
2019-08-08 11:07:09 +0530

    fourth commit

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: "vearn_db"
+  mongoURI:
+    "mongodb+srv://brad:brad@vcluster0-roq8c.mongodb.net/test?retryWrites=true&w=majority"
   //mongodb+srv://brad:brad@vcluster0-roq8c.mongodb.net/test?retryWrites=true&w=majority
 };
